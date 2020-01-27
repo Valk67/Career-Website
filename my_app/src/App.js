1d09a96dc6926e598524948bec5cc8807aa8d5b1
Valk67 <bsunds@gmail.com>
2020-01-27 01:27:48 -0800

    version 1.0

--- my_app/src/App.js
+++ my_app/src/App.js
@@ -3,133 +3,196 @@ import logo from './logo.svg';
 import './App.css';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
-import Container from '@material-ui/core/Container';
+// import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import ButtonGroup from '@material-ui/core/ButtonGroup';
-import download from 'react-mdl';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import DownloadButton from './screens/resume'
+import Avatar from '@material-ui/core/Avatar';
 import agent_file from './downloadableFiles/BradleySundsbarmResumeCS2020.pdf';
-// import {
-//   Toolbar,
-//   IconButton,
-//   classes,
-//   MenuIcon,
-//   Typography,
-// } from '@material-ui/core/';
+import myAvatar from './downloadableFiles/self.jpeg';
+import chicoState from './downloadableFiles/CSUCHICO.png';
+import Paper from '@material-ui/core/Paper';
+import FormLabel from '@material-ui/core/FormLabel';
+import ResizeImage from 'react-resize-image'
+
+
 
 const useStyles = makeStyles(theme => ({
+  toolbar: theme.mixins.toolbar,
   root: {
     flexGrow: 1,
   },
+  paper: {
+    padding: theme.spacing(9),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+    // alignItems: 'center',
+    // justify: 'center',
+  },
+  paper2: {
+    marginTop: 35,
+    padding: theme.spacing(9),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+    // alignItems: 'center',
+    // justify: 'center',
+  },
+  control: {
+    padding: theme.spacing(2),
+  },
   menuButton: {
     marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 2,
   },
   buttonNav:{
     backgroundColor:'#282c34',
     padding: 12,
     color: "white",
     // flexGrow: 4,
+  },
+  // card: {
+  //   minWidth: 275,
+  //   alignItems: 'left',
+  //   // justifyContent: 'space-between'
+  //   // justifyContent: "left"
+  // },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  large: {    
+    // justifyContent: 'center',
+    // alignItems: 'center',
+    marginTop: 33,
+    marginBottom: 33,
+    marginLeft: 110,
+    width: theme.spacing(20),
+    height: theme.spacing(20),
+  },
+  gridCards: { //controls the container for the 2 main grids
+    justifyContent: 'center',
+    marginTop: 5,
+  },
+  control: {
+    padding: 20,
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+    marginTop: -30
 
   },
+  bottomGrid: {
+    marginTop: 0,
+    // flexDirection: 'row',
+    // justifyContent: 'space-between',
+  }
 }));
 
-{/* <a href="mailto:brian.atiyeh@gmail.com" 
-class="btn btn-layered offset square" role="button" 
-aria-pressed="true">hire me</a> */}
 
 function App() {
 
   const classes = useStyles();
+  const bull = <span className={classes.bullet}>•</span>;
   return (
-    // <Container fixed>
     <div className="App">
-      <div classes = {classes.root}>
-        <AppBar position="fixed">
-          <Toolbar>
+     <div className={classes.root}>
+       
+      <AppBar position="fixed" color="primary">
+        <Toolbar>
+          {/* <Typography variant="h6" className={classes.title}>
+            Bradley Sundsbarm
+          </Typography> */}
           <Grid
-            justify="center" 
+            // justify="flex-end" 
+            justify="center"
             container 
-            // spacing={24}
           >
-            {/* <Typography variant="h6" className={classes.title}>
-              Bradley Sundsbarm
-            </Typography> */}
-            <Grid item>
-            <Button variant="contained"
-                    className={classes.buttonNav}
-                    href={agent_file} download="BradleySundsbarmResume2020.pdf"
-            >
-                Download My Resume
-            </Button>
-            </Grid>
-            &nbsp;&nbsp;&nbsp;
-            <Grid item>
-            <Button variant="contained" 
-                    className={classes.buttonNav}
-                    href="mailto:bsunds@gmail.com"
-            >
-                Hire Me
-            </Button>
-            </Grid>
-            </Grid>
-          </Toolbar>
-        </AppBar>
-      </div>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Bradley Sundsbarm
-        </p>
-        
-        <ButtonGroup variant="contained" color="primary" aria-label="contained primary button group">
+              <Grid item>
+                <Button variant="contained"
+                        className={classes.buttonNav}
+                        href={agent_file} download="BradleySundsbarmResume2020.pdf"
+                >
+                    Download My Resume
+                </Button>
+              </Grid>
+              &nbsp;&nbsp;&nbsp;
+              <Grid item>
+                <Button variant="contained" 
+                        className={classes.buttonNav}
+                        href="mailto:bsunds@gmail.com"
+                >
+                    Email Me
+                </Button>
+              </Grid>
+          </Grid>
+        </Toolbar>
+      </AppBar>
 
-            <Button variant="contained" 
-                    color="primary" 
-                    href="https://github.com/Valk67?tab=repositories"
-                    target="_blank"
-                    rel="noopener noreferrer"
-            >
-              Github
-            </Button>
-            <Button variant="contained" 
-                    color="primary" 
-                    href="https://www.linkedin.com/in/bradley-sundsbarm-b3834a133/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-            >
-              Linkedin
-            </Button>
-            <Button variant="contained" 
-                    color="primary" 
-                    href="https://www.facebook.com/bradley.sundsbarm"
-                    target="_blank"
-                    rel="noopener noreferrer"
-            >
-              Facebook
-            </Button>
-        </ButtonGroup>
-         
-           {/* <a
-            className="App-link"
-            href="https://www.linkedin.com/in/bradley-sundsbarm-b3834a133/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Linkedin
-          </a> */}
+     <div className={classes.toolbar} /> 
+
+      <Grid container spacing={7} className={classes.gridCards}>
+        <Grid item xs={4}>
+          <Paper className={classes.paper}>
+            <h1> Bradley Sundsbarm's Career Website, Welcomes You!</h1>
+            <Avatar alt="Bradley Sundsbarm" src={myAvatar} className={classes.large} />
+            Former Air Traffic Controller in the United States Navy,
+            currently persuing Software Engineering as my new career.
+          </Paper>
+        </Grid>
+        <Grid item xs={4}>
+          <Paper className={classes.paper2}>
+            <h3>
+              Built with React
+            </h3>
+            <img src={logo} className="App-logo" alt="logo" />
+            <ButtonGroup variant="contained" color="primary" aria-label="contained primary button group">
+
+              <Button color="primary" 
+                      href="https://github.com/Valk67?tab=repositories"
+                      target="_blank"
+                      rel="noopener noreferrer"
+              >
+                Github
+              </Button>
+              <Button color="primary" 
+                      href="https://www.linkedin.com/in/bradley-sundsbarm-b3834a133/"
+                      target="_blank"
+                      rel="noopener noreferrer"
+              >
+                Linkedin
+              </Button>
+              <Button color="primary" 
+                      href="https://www.facebook.com/bradley.sundsbarm"
+                      target="_blank"
+                      rel="noopener noreferrer"
+              >
+                Facebook
+              </Button>
+            </ButtonGroup>
+            <p>
+              {bull}All links will open a new tab and redirect.
+            </p>
           
-      </header>
+          </Paper>
+        </Grid>  
+        <Grid item xs={6}>
+        <Paper className={classes.control}>
+          <Grid className={classes.bottomGrid}>
+            <Grid item>
+            <img src={chicoState} className="App-chico" alt="logo" />
+              Bachelor of Science in Computer Science. Chico State University, DEC 2019.
+              
+            </Grid>
+          </Grid>
+        </Paper>
+      </Grid>
+      </Grid>
+     </div>
     </div>
-    // </Container>
   );
 }
 
+   
 
 export default App;
+
